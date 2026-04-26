5076c1e16b2af898160cba0b88c65f2e79eb4dad
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2026-04-27 00:22:52 +0700

    Fix(fstab): Use Halium-specific fstab for Ubuntu Touch build
    - Add halium.fstab based on real device partition map.
    - Override default fstab in halium_d2s.mk to ensure correct mounting.

--- sources/device/samsung/d2s/halium_d2s.mk
+++ sources/device/samsung/d2s/halium_d2s.mk
@@ -3,3 +3,11 @@ $(call inherit-product, $(LOCAL_DIR)/lineage_d2s.mk)
 
 # Halium-specific overrides
 PRODUCT_NAME := halium_d2s
+
+# Halium fstab
+PRODUCT_COPY_FILES += \
+    halium/halium.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.exynos9820 \
+    halium/halium.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.exynos9825 \
+    halium/halium.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos9820 \
+    halium/halium.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos9825
+
